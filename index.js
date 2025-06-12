bf48988673225d2ae98f7493d0a1e3d2a8ed2611
annet <annetgnyabuto@gmail.com>
2025-06-12 13:39:32 +0300

    use of filter function

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(drivers, namesToMatch){
+       return drivers.filter(function(driver){
+       return driver.toLowerCase() === namesToMatch.toLowerCase();
+    });
+}
+//driversLower hols the lowercase name
+//lettersToLower holds lowercase search term
+function fuzzyMatch(drivers, lettersToMatch){
+    return drivers.filter(function(driver){
+        const driverLower = driver.toLowerCase();
+        const lettersToLower = lettersToMatch.toLowerCase();
+        return driverLower.startsWith(lettersToLower);
+    });
+}
+const driver=[
+    {
+        name: "John",
+        hometown: "Texas"
+    }
+];
+function matchName(drivers, namesToMatch){
+    return drivers.filter(function(driver){
+        return driver.name === namesToMatch
+    });
+}
